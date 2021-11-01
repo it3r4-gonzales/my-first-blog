386ad7c09240e4b3c67c890f483942e1d148a677
gonzales <gonzalesjohnromil57@gmail.com>
2021-11-01 22:27:02 +0800

    trial

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,5 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+#STATIC_ROOT = "/home/myusername/myproject/static"
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "\User\Gonzales\djangogirls"
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
