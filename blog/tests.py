57880716d4e0e80b7bfecd468cc99fee0a441435
gonzales <gonzalesjohnromil57@gmail.com>
2021-12-02 11:02:33 +0800

    forms

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.chrome.webdriver import WebDriver
+
